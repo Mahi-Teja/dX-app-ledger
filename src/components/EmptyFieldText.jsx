d9d3ab60814652b4747ac4578cc1cc73c9cb705e
Mahi-Teja <mahineeli123@gmail.com>
2025-08-05 21:40:49 +0530

    Add EmptyWithAction component to display message when no categories are present

--- src/components/EmptyFieldText.jsx
+++ src/components/EmptyFieldText.jsx
@@ -11,3 +11,23 @@ const EmptyFieldText = ({ children, customClass }) => {
 };
 
 export default EmptyFieldText;
+
+export const EmptyWithAction = ({
+  message = "No items found",
+  icon = "🎯",
+  buttonText = "Add New",
+  onClick,
+}) => (
+  <div className="flex flex-col items-center justify-center  w-full py-16 text-center text-gray-600">
+    <div className="bg-gradient-to-b from-[#c2e8c8] to-[#9394bc] backdrop-blur-3xl  rounded-xl p-40">
+      <div className="text-6xl mb-4">{icon}</div>
+      <p className="text-lg mb-4">{message}</p>
+      <button
+        onClick={onClick}
+        className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+      >
+        {buttonText}
+      </button>
+    </div>
+  </div>
+);
