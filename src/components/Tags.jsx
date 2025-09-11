1c29660e73cd01ea449a337eab5a00a7e9886d64
Mahi-Teja <mahineeli123@gmail.com>
2025-09-12 00:28:48 +0530

    refactor: streamline DateNav component layout and improve button styles; enhance TodaysStats for better clarity and responsiveness; tidy up Tags component structure; update Button1 for improved functionality and styling

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -2,9 +2,11 @@ export const Tags = ({ title, close }) => {
   return (
     <div className="bg-gray-100 rounded p-1 mx-0.5 w-fit flex">
       {title}
-      <button onClick={(e) => close(e)} className="px-1">
-        x
-      </button>
+      {close && (
+        <button onClick={(e) => close(e)} className="px-1">
+          x
+        </button>
+      )}
     </div>
   );
 };
--- src/components/buttons/button1.jsx
+++ src/components/buttons/button1.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
-
+ 
 export const Button1 = ({
   children,
+  type,
   className = "",
   handleClick = () => {},
   title = "",
 }) => {
   return (
     <button
+      type={type}
       className={`bg-indigo-600 rounded p-2 m-2 cursor-pointer text-white font-semibold ${className}`}
       onClick={(e) => {
         e.preventDefault();
@@ -19,6 +20,15 @@ export const Button1 = ({
     </button>
   );
 };
+export const SubmitButton = ({ children, title, type }) => {
+  <button
+    type={type}
+    className={`bg-indigo-600 h-3 rounded p-2 m-2 cursor-pointer text-white font-semibold`}
+    title={title}
+  >
+    {children}
+  </button>;
+};
 
 export const CustomButton2 = ({
   onClickHandler,
@@ -29,7 +39,7 @@ export const CustomButton2 = ({
   return (
     <button
       onClick={(e) => onClickHandler(e)}
-      className={`px-4 min-w-28 mx-1 py-2   text-sm font-semibold  rounded  transition-all duration-200 shadow-md
+      className={`px-4 w-fit md:min-w-28 mx-1 py-2   text-sm font-semibold  rounded  transition-all duration-200 shadow-md
               ${
                 noFill
                   ? " border-blue-600 border text-blue-600"
